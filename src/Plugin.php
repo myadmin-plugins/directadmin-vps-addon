7fb4e55cc73b73c2142fc547a38cf972a518b17d
Joe Huss <detain@interserver.net>
2017-06-10 15:26:43 -0400

    work on the activation code

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,7 +15,14 @@ class Plugin {
 		$addon = new \Addon();
 		$addon->set_module('vps')->set_text('DirectAdmin')->set_cost(VPS_DA_COST)
 			->set_require_ip(true)->set_enable(function() {
+				$service_info = $service_order->get_service_info();
+				$settings = get_module_settings($service_order->get_module());
 				require_once 'include/licenses/license.functions.inc.php';
+				$pass = vps_get_password($service_info[$settings['PREFIX'].'_id']);
+				function_requirements('directadmin_get_best_type');
+				function_requirements('activate_directadmin');
+				$ostype = directadmin_get_best_type($module, $service_info[$settings['PREFIX'] . '_type'], $service_info, $service_extra);
+				$result = activate_directadmin($service_info[$settings['PREFIX'].'_ip'], $ostype, $pass, $GLOBALS['tf']->accounts->cross_reference($service_info[$settings['PREFIX'] . '_custid']), $module . $service_info[$settings['PREFIX'].'_id']);
 			})->set_disable(function() {
 			})->register();
 		$service->add_addon($addon);
