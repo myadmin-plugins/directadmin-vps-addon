c7183f69dd9727dd31f12086012a8752e9a88cde
Joe Huss <detain@interserver.net>
2017-06-17 08:49:09 -0400

    updates completing some missing class callback settings , fixing a few places thta needed minor changes

--- src/Plugin.php
+++ src/Plugin.php
@@ -31,8 +31,8 @@ class Plugin {
 			->set_text('DirectAdmin')
 			->set_cost(VPS_DA_COST)
 			->set_require_ip(true)
-			->set_enable(['Detain\MyAdminVpsDirectadmin\Plugins', 'Enable'])
-			->set_disable(['Detain\MyAdminVpsDirectadmin\Plugins', 'Disable'])
+			->set_enable([__CLASS__, 'Enable'])
+			->set_disable([__CLASS__, 'Disable'])
 			->register();
 		$service->add_addon($addon);
 	}
