112cfd45ba473f2bd524e21ddba42ecd0b46dfa3
Joe Huss <detain@interserver.net>
2017-06-24 07:19:31 -0400

    camelCase type refactoring

--- src/Plugin.php
+++ src/Plugin.php
@@ -27,7 +27,7 @@ class Plugin {
 		$service = $event->getSubject();
 		function_requirements('class.Addon');
 		$addon = new \Addon();
-		$addon->set_module('vps')
+		$addon->setModule('vps')
 			->set_text('DirectAdmin')
 			->set_cost(VPS_DA_COST)
 			->set_require_ip(TRUE)
