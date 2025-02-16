a7ed84ec631c5593c83be0a2f1a7956c01d9acfe
Joe Huss <detain@interserver.net>
2025-02-15 21:53:17 -0500

    updates to settings

--- src/Plugin.php
+++ src/Plugin.php
@@ -115,6 +115,8 @@ class Plugin
          * @var \MyAdmin\Settings $settings
          **/
         $settings = $event->getSubject();
+        $settings->setTarget('module');
         $settings->add_text_setting(self::$module, _('Addon Costs'), 'vps_da_cost', _('VPS DirectAdmin License'), _('This is the cost for purchasing a direct admin license on top of a VPS.'), $settings->get_setting('VPS_DA_COST'));
+        $settings->setTarget('global');
     }
 }
