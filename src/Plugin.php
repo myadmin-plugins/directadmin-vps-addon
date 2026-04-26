7a287fd4686913aafe08b70f8c41888e83962870
Joe Huss <detain@interserver.net>
2026-04-26 15:21:48 -0400

    updates migrating off global tf handler to the MyAdminApp class handler

--- src/Plugin.php
+++ src/Plugin.php
@@ -85,7 +85,7 @@ class Plugin
         function_requirements('activate_directadmin');
         $serviceExtra = run_event('parse_service_extra', $serviceInfo[$settings['PREFIX'].'_extra'], self::$module);
         $ostype = directadmin_get_best_type(self::$module, $serviceInfo[$settings['PREFIX'].'_type'], $serviceInfo, $serviceExtra);
-        activate_directadmin($serviceInfo[$settings['PREFIX'].'_ip'], $ostype, $pass, $GLOBALS['tf']->accounts->cross_reference($serviceInfo[$settings['PREFIX'].'_custid']), self::$module.$serviceInfo[$settings['PREFIX'].'_id']);
+        activate_directadmin($serviceInfo[$settings['PREFIX'].'_ip'], $ostype, $pass, \MyAdmin\App::accounts()->cross_reference($serviceInfo[$settings['PREFIX'].'_custid']), self::$module.$serviceInfo[$settings['PREFIX'].'_id']);
     }
 
     /**
